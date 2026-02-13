4ab36857e97eb337a2fd52353f45075941882323
Kushagra <kushagrasinghchauhan6@gmail.com>
2026-02-13 23:02:55 +0530

    up5

--- backend/routes/general.js
+++ backend/routes/general.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Task = require("../models/Task");
+const Task = require("../models/tasks");
 const requireAuth = require("../middleware/auth-middleware");
 
 // 🔐 protect all task routes
