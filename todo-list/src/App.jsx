8ff89d09f9802b15a5c79dc09a12b72a8674b765
Kushagra <kushagrasinghchauhan6@gmail.com>
2026-02-14 11:28:15 +0530

    up11

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Load from "./components/task-list";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
 import Loginedpage from "./pages/logined-page";
